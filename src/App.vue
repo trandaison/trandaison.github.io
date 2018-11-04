8b564c0c164da28a76fd023a8f443189480d611b
Son Tran <sontd.it@gmail.com>
2018-11-04 20:31:43 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+import './main.scss'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
---
+++ src/components/Home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <a href="#">Click here</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  style: '../../../scss/home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
