06f058f143015ac16e16a53c5802f8250841b69c
Son Tran <sontd.it@gmail.com>
2018-11-06 23:05:46 +0700

    heading

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,16 +1,88 @@
 <template>
   <div class="container">
-    <a href="#">Click here</a>
+    <div class="content">
+      <section class="heading">
+        <div class="avatar">
+          <img class="avatar__image" src="@/assets/img/avatar.jpg" alt="Avatar">
+        </div>
+        <div class="general">
+          <h1 class="general__full-name">Trần Đại Sơn</h1>
+          <ul class="general__basic-info">
+            <li class="general__info">
+              <div class="general__info__icon">
+                <span class="fa-icon"><v-icon name="map-marker-alt" scale="2"/></span>
+              </div>
+              <div class="description right hidden">{{ address }}</div>
+            </li>
+            <li class="general__info">
+              <div class="general__info__icon">
+                <span class="fa-icon"><v-icon name="at" scale="2"/></span>
+              </div>
+              <div class="description right hidden"><a :href="`mailto:${email}`" v-text="email"/></div>
+            </li>
+            <li class="general__info">
+              <div class="general__info__icon">
+                <span class="fa-icon"><v-icon name="mobile-alt" scale="2"/></span>
+              </div>
+              <div class="description left hidden">{{ phone }}</div>
+            </li>
+            <li class="general__info">
+              <div class="general__info__icon">
+                <span class="fa-icon"><v-icon name="calendar-alt" scale="2"/></span>
+              </div>
+              <div class="description left hidden">12/03/1994</div>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section class="main-content">
+        <div class="left-side">
+
+        </div>
+        <div class="right-side">
+
+        </div>
+      </section>
+      <section class="footer">
+        <ul class="contact">
+          <li class="contact__item">
+            <a href="https://trandaison.github.io">
+              <span class="fa-icon"><v-icon name="laptop-code"/></span> {{ website }}
+            </a>
+          </li>
+          <li class="contact__item">
+            <a href="https://github.com/trandaison">
+              <span class="fa-icon"><v-icon name="brands/github"/></span> {{ github }}
+            </a>
+          </li>
+          <li class="contact__item">
+            <a href="https://www.facebook.com/sontran.ds">
+              <span class="fa-icon"><v-icon name="brands/facebook"/></span> {{ facebook }}
+            </a>
+          </li>
+          <li class="contact__item">
+            <a :href="`mailto:${email}`">
+              <span class="fa-icon"><v-icon name="brands/google-plus-square"/></span> {{ google }}
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  style: '../../../scss/home',
+  name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      address: '135 Nguyen Duc Trung, Thanh Khe dist., Danang',
+      phone: '+84 706 213188',
+      email: 'sontd.it@gmail.com',
+      github: 'trandaison',
+      website: 'trandaison.github.io',
+      facebook: 'sontran.ds',
+      google: 'sontd.it'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
 Vue.config.productionTip = false
+
+Vue.component('v-icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
