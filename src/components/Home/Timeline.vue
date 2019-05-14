39dbabb1b1f8de7dd674b4bfbbf706fc424c664b
Son Tran <sontd.it@gmail.com>
2019-05-14 15:40:01 +0700

    update cv

---
+++ src/components/Home/Timeline.vue
@@ -0,0 +1,237 @@
+<template lang="html">
+  <div class="box">
+    <ul>
+      <li>
+        <span></span>
+        <div class="title">Video sharing platform (Front End)</div>
+        <div class="info">
+          <div class="project-description">
+            <p class="text">
+              Build the front end application on VueJS.
+            </p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Front End Engineer (Team leader)</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>3-6</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Jun 2019</span>
+          <span>Oct 2018</span>
+        </div>
+      </li>
+      <li>
+        <span></span>
+        <div class="title">Video sharing platform (Back End)</div>
+        <div class="info">
+          <div class="project-description">
+            <p class="text">
+              A video social network (likes Youtube) using <a href="https://uiza.io" target="_blank">Uiza</a> SDK.
+              Back end server was developed on Ruby on Rails.
+            </p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Ruby developer</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>4</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Oct 2018</span>
+          <span>Aug 2018</span>
+        </div>
+      </li>
+      <li>
+        <span></span>
+        <div class="title">Kyorisoku</div>
+        <div class="info">
+          <div class="project-description">
+            <a href="https://www.mapion.co.jp/" target="_blank">
+              <img class="thumb" src="@/assets/img/kyorisoku.png" alt="Mapion Logo">
+            </a>
+            <p class="text">
+              Rebuild a map and mesurement for <a target="_blank" href="https://www.mapion.co.jp/">mapion.co.jp</a>.<br>
+              Developed on VueJS, LeafletJS and Ruby on Rails.
+            </p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Front End Engineer (Team leader)</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>2</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Jun 2018</span>
+          <span>Jan 2018</span>
+        </div>
+      </li>
+      <li>
+        <span></span>
+        <div class="title">Mapion Web Maintenance</div>
+        <div class="info">
+          <div class="project-description">
+            <a href="https://www.mapion.co.jp/" target="_blank">
+              <img class="thumb" src="@/assets/img/mapion.png" alt="Mapion Logo">
+            </a>
+            <p class="text">
+              Maintain the website <a target="_blank" href="https://www.mapion.co.jp/">mapion.co.jp</a><br>
+              It is map and weather services from Mapion, developed on Ruby and Mayaa.
+            </p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Ruby Developer (Team leader)</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>2</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Dec 2017</span>
+          <span>Jun 2017</span>
+        </div>
+      </li>
+      <li>
+        <span></span>
+        <div class="title">Aruku& Ranking Event</div>
+        <div class="info">
+          <div class="project-description">
+            <img class="thumb" src="@/assets/img/arukuto_app_logo.jpg" alt="Aruku& Logo">
+            <p class="text">Develop a new feature for <a target="_blank" href="https://apps.apple.com/JP/app/id1165290449?mt=8">aruku&</a> - ranking event. Rank players in order of points by seasons.</p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Ruby Developer (Team leader)</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>3</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Aug 2017</span>
+          <span>Jun 2017</span>
+        </div>
+      </li>
+      <li>
+        <span></span>
+        <div class="title">Aruku& Column</div>
+        <div class="info">
+          <div class="project-description">
+            <img class="thumb" src="@/assets/img/arukuto_app_logo.jpg" alt="Aruku& Logo">
+            <p class="text">Develop column feature and push notification for <a target="_blank" href="https://apps.apple.com/JP/app/id1165290449?mt=8">aruku&</a> application.</p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Ruby Developer</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>4-6</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Jun 2017</span>
+          <span>Mar 2017</span>
+        </div>
+      </li>
+      <li>
+        <span></span>
+        <div class="title">Aruku& CoTenant</div>
+        <div class="info">
+          <div class="project-description">
+            <img class="thumb" src="@/assets/img/arukuto_app_logo.jpg" alt="Aruku& Logo">
+            <p class="text">Develop the admin page for <a target="_blank" href="https://apps.apple.com/JP/app/id1165290449?mt=8">aruku&</a>'s custommers, using multi-tenant and subdomain.</p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Ruby Developer</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>4-6</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Mar 2017</span>
+          <span>Dec 2016</span>
+        </div>
+      </li>
+      <li>
+        <span></span>
+        <div class="title">Aruku& API</div>
+        <div class="info">
+          <div class="project-description">
+            <img class="thumb" src="@/assets/img/arukuto_app_logo.jpg" alt="Aruku& Logo">
+            <p class="text">Write API and Spec for <a target="_blank" href="https://apps.apple.com/JP/app/id1165290449?mt=8">aruku&</a>, it is an earn prize for walking application in Japan.</p>
+          </div>
+          <table class="project-details">
+            <tr>
+              <th>Role:</th>
+              <td>Ruby Developer</td>
+            </tr>
+            <tr>
+              <th>Team size:</th>
+              <td>6-10</td>
+            </tr>
+          </table>
+        </div>
+        <div class="time">
+          <span>Jan 2017</span>
+          <span>Dec 2016</span>
+        </div>
+      </li>
+      <div class="arrow" id="btn1" @click="scrollToTop">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 284.929 284.929" style="enable-background:new 0 0 284.929 284.929;" xml:space="preserve">
+          <g>
+            <g>
+              <path d="M135.899,167.877c1.902,1.902,4.093,2.851,6.567,2.851s4.661-0.948,6.562-2.851L282.082,34.829    c1.902-1.903,2.847-4.093,2.847-6.567s-0.951-4.665-2.847-6.567L267.808,7.417c-1.902-1.903-4.093-2.853-6.57-2.853    c-2.471,0-4.661,0.95-6.563,2.853L142.466,119.622L30.262,7.417c-1.903-1.903-4.093-2.853-6.567-2.853    c-2.475,0-4.665,0.95-6.567,2.853L2.856,21.695C0.95,23.597,0,25.784,0,28.262c0,2.478,0.953,4.665,2.856,6.567L135.899,167.877z" fill="#FFFFFF"/>
+              <path d="M267.808,117.053c-1.902-1.903-4.093-2.853-6.57-2.853c-2.471,0-4.661,0.95-6.563,2.853L142.466,229.257L30.262,117.05    c-1.903-1.903-4.093-2.853-6.567-2.853c-2.475,0-4.665,0.95-6.567,2.853L2.856,131.327C0.95,133.23,0,135.42,0,137.893    c0,2.474,0.953,4.665,2.856,6.57l133.043,133.046c1.902,1.903,4.093,2.854,6.567,2.854s4.661-0.951,6.562-2.854l133.054-133.046    c1.902-1.903,2.847-4.093,2.847-6.565c0-2.474-0.951-4.661-2.847-6.567L267.808,117.053z" fill="#FFFFFF"/>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timeline',
+
+  methods: {
+    scrollToTop () {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+</script>
--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -8,73 +8,197 @@
         <div class="general">
           <h1 class="general__full-name">Trần Đại Sơn</h1>
           <ul class="general__basic-info">
-            <li class="general__info">
+            <li
+              v-for="(item, index) in basicInfo"
+              :key="item.name"
+              :title="item.title"
+              class="general__info"
+              @mouseover="onMouseOverInfo"
+              @mouseleave="onMouseLeaveInfo"
+            >
               <div class="general__info__icon">
-                <span class="fa-icon"><v-icon name="map-marker-alt" scale="2"/></span>
+                <span class="fa-icon"><v-icon :name="item.faIcon" scale="2"/></span>
               </div>
-              <div class="description right hidden">{{ address }}</div>
+              <div
+                class="description hidden"
+                :class="[index < basicInfo.length / 2 ? 'right' : 'left']"
+              >
+                {{ item.value }}
+              </div>
             </li>
-            <li class="general__info">
+            <li
+              class="general__info viblo"
+              title="Viblo blog"
+              @mouseover="onMouseOverInfo"
+              @mouseleave="onMouseLeaveInfo"
+              @click="open('https://viblo.asia/u/tran.dai.son')"
+            >
               <div class="general__info__icon">
-                <span class="fa-icon"><v-icon name="at" scale="2"/></span>
+                <img src="@/assets/img/viblo.png" alt="Viblo">
               </div>
-              <div class="description right hidden"><a :href="`mailto:${email}`" v-text="email"/></div>
-            </li>
-            <li class="general__info">
-              <div class="general__info__icon">
-                <span class="fa-icon"><v-icon name="mobile-alt" scale="2"/></span>
+              <div
+                class="description hidden"
+                :class="[index < basicInfo.length / 2 ? 'right' : 'left']"
+              >
+                @tran.dai.son
               </div>
-              <div class="description left hidden">{{ phone }}</div>
-            </li>
-            <li class="general__info">
-              <div class="general__info__icon">
-                <span class="fa-icon"><v-icon name="calendar-alt" scale="2"/></span>
-              </div>
-              <div class="description left hidden">12/03/1994</div>
             </li>
           </ul>
         </div>
       </section>
       <section class="main-content">
         <div class="left-side">
-
+          <div class="row">
+            <div class="sub-content">
+              <h2>About me</h2>
+              <p>A back-end developer</p>
+              <p>Interested in VueJS</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="sub-content">
+              <h2>Experiences</h2>
+              <div v-for="item in experiences" :key="item.company" class="sub-content__block">
+                <div class="sub-content__logo">
+                  <a :href="item.website" target="_blank">
+                    <img :src="item.logo" :alt="item.company">
+                  </a>
+                </div>
+                <div class="sub-content__body experience">
+                  <div class="primary-content">
+                    <h3 class="name">
+                      <a :href="item.website" target="_blank">{{ item.company }}</a>
+                    </h3>
+                    <span class="location">{{ item.location }}</span>
+                  </div>
+                  <div class="secondary-content">
+                    <span class="time">{{ `${item.from} - ${item.to}` }}</span>
+                    <p class="major">{{ item.position }}</p>
+                    <p class="description">{{ item.description }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="sub-content">
+              <h2>Education</h2>
+              <div v-for="item in education" :key="item.name" class="sub-content__block">
+                <div class="sub-content__logo">
+                  <a :href="item.website" target="_blank">
+                    <img :src="item.logo" :alt="item.name">
+                  </a>
+                </div>
+                <div class="sub-content__body">
+                  <div class="primary-content">
+                    <h3 class="name">
+                      <a :href="item.website" target="_blank">{{ item.name }}</a>
+                    </h3>
+                    <span class="time">{{ `${item.from} - ${item.to}` }}</span>
+                  </div>
+                  <div class="secondary-content">
+                    <p class="major">{{ item.subject }}</p>
+                    <p class="description">{{ item.details }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="sub-content">
+              <h2>Skills</h2>
+              <div class="sub-content__skills">
+                <table class="sub-content__table">
+                  <tr>
+                    <th>
+                      <div class="inner-text">Technical</div>
+                    </th>
+                  </tr>
+                  <tr v-for="skill in skills" :key="skill.id">
+                    <td>
+                      <div class="inner-text" :title="skill.name">
+                        <input
+                          :class="['slider', skill.id]"
+                          :title="skill.name"
+                          type="range"
+                          min="0"
+                          max="10"
+                          v-model="skill.value"
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>
+                      <div class="inner-text">General Skills</div>
+                    </th>
+                  </tr>
+                  <tr v-for="skill in generalSkills" :key="skill">
+                    <td>
+                      <div class="inner-text">
+                        {{ skill }}
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="right-side">
-
+          <Timeline/>
         </div>
       </section>
       <section class="footer">
         <ul class="contact">
           <li class="contact__item">
-            <a href="https://trandaison.github.io">
+            <a href="https://trandaison.github.io" target="_blank">
               <span class="fa-icon"><v-icon name="laptop-code"/></span> {{ website }}
             </a>
           </li>
           <li class="contact__item">
-            <a href="https://github.com/trandaison">
+            <a href="https://github.com/trandaison" target="_blank">
               <span class="fa-icon"><v-icon name="brands/github"/></span> {{ github }}
             </a>
           </li>
           <li class="contact__item">
-            <a href="https://www.facebook.com/sontran.ds">
+            <a href="https://www.facebook.com/sontran.ds" target="_blank">
               <span class="fa-icon"><v-icon name="brands/facebook"/></span> {{ facebook }}
             </a>
           </li>
           <li class="contact__item">
             <a :href="`mailto:${email}`">
-              <span class="fa-icon"><v-icon name="brands/google-plus-square"/></span> {{ google }}
+              <span class="fa-icon"><v-icon name="envelope"/></span> {{ google }}
             </a>
           </li>
         </ul>
       </section>
     </div>
+
+    <div class="download-link">
+      <a class="button" href="#" @click.prevent.stop="downloadCV">
+        <span class="fa-icon"><v-icon name="file-download"/></span>
+        Download CV
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
+import dutLogo from '@/assets/img/dut_logo.png'
+import atLogo from '@/assets/img/at_logo.png'
+import framgiaLogo from '@/assets/img/framgia_logo.png'
+import sunLogo from '@/assets/img/sun-logo.jpg'
+import Timeline from './Timeline'
+
 export default {
   name: 'Home',
   data () {
+    const current = new Intl.DateTimeFormat('en-US', {
+      month: 'short',
+      year: 'numeric'
+    }).format(new Date())
+
     return {
       address: '135 Nguyen Duc Trung, Thanh Khe dist., Danang',
       phone: '+84 706 213188',
@@ -82,7 +206,135 @@ export default {
       github: 'trandaison',
       website: 'trandaison.github.io',
       facebook: 'sontran.ds',
-      google: 'sontd.it'
+      google: 'sontd.it',
+      basicInfo: [
+        {
+          name: 'address',
+          title: 'Address',
+          value: '135 Nguyen Duc Trung, Thanh Khe dist., Danang',
+          faIcon: 'map-marker-alt'
+        }, {
+          name: 'phone',
+          title: 'Mobile',
+          value: '+84 706 213188',
+          faIcon: 'at'
+        }, {
+          name: 'email',
+          title: 'Email',
+          value: 'sontd.it@gmail.com',
+          faIcon: 'mobile-alt'
+        }
+      ],
+      experiences: [
+        {
+          company: 'Framgia Inc.',
+          location: 'Danang',
+          from: 'Dec 2016',
+          to: 'March 2019',
+          position: 'Ruby developer',
+          description: null,
+          website: 'https://sun-asterisk.vn/',
+          logo: framgiaLogo
+        }, {
+          company: 'Sun*',
+          location: 'Danang',
+          from: 'March 2019',
+          to: `${current}`,
+          position: 'Ruby developer',
+          description: null,
+          website: 'https://sun-asterisk.vn/',
+          logo: sunLogo
+        }
+      ],
+      education: [
+        {
+          name: 'DUT',
+          website: 'http://dut.udn.vn/KhoaCNTT',
+          from: '2012',
+          to: '2017',
+          subject: 'IT Engineer',
+          details: 'Class of 2012-2017',
+          logo: dutLogo
+        }, {
+          name: 'Asian Tech',
+          website: 'https://asiantech.vn/',
+          from: 'June 2016',
+          to: 'Aug 2016',
+          subject: 'PHP Intern',
+          details: '',
+          logo: atLogo
+        }, {
+          name: 'Framgia Inc.',
+          website: 'https://sun-asterisk.vn/',
+          from: 'Oct 2016',
+          to: 'Dec 2016',
+          subject: 'Ruby Intern',
+          details: '',
+          logo: framgiaLogo
+        }
+      ],
+      skills: [
+        { name: 'HTML', value: 10, id: 'html' },
+        { name: 'JS/jQuery', value: 10, id: 'js' },
+        { name: 'ECMAScript 6', value: 10, id: 'es6' },
+        { name: 'VueJS', value: 10, id: 'vuejs' },
+        { name: 'CSS-SCSS', value: 9, id: 'css' },
+        { name: 'MySQL', value: 9, id: 'mysql' },
+        { name: 'Ruby', value: 8, id: 'ruby' },
+        { name: 'NodeJS', value: 8, id: 'nodejs' },
+        { name: 'PHP', value: 6, id: 'php' },
+        { name: 'Java', value: 6, id: 'java' },
+        { name: 'AngularJS', value: 5, id: 'angularjs' }
+      ],
+      generalSkills: [
+        'Communication',
+        'Problem Solving',
+        'Leadership',
+        'Teamwork',
+        'Scrum',
+        'Technical Seminar',
+        'VueJS Basic Training'
+      ]
+    }
+  },
+
+  components: {
+    Timeline
+  },
+
+  methods: {
+    onMouseOverInfo (e) {
+      const target = e.target
+      const others = document.querySelectorAll('.general .general__info')
+      for (let i = 0; i < others.length; i++) {
+        const li = others[i]
+        if (li !== target) {
+          li.classList.add('invisible')
+        }
+      }
+
+      target.classList.add('hovering')
+    },
+
+    onMouseLeaveInfo (e) {
+      const target = e.target
+      const others = document.querySelectorAll('.general .general__info')
+      for (let i = 0; i < others.length; i++) {
+        const li = others[i]
+        if (li !== target) {
+          li.classList.remove('invisible')
+        }
+      }
+
+      target.classList.remove('hovering')
+    },
+
+    open (url) {
+      window.open(url, '_blank')
+    },
+
+    downloadCV () {
+      alert('Hãy liên hệ Sơn để nhận được CV.\nPlease contact Son to get the lastest CV.')
     }
   }
 }
